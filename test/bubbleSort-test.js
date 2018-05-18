ca9d3d1a2717d7e3dee9f6b412538284079d5765
Marika Ross <marika.ross@gmail.com>
2018-05-18 17:43:56 -0600

    Finalized insertionSort method '

--- test/bubbleSort-test.js
+++ test/bubbleSort-test.js
@@ -1,5 +1,43 @@
+import { expect } from 'chai';
+import { bubbleSort } from '../lib/bubbleSort.js';
+
+let bubble = bubbleSort
 //should be a function bubbleSort()
 //should take a small array as an argument bubbleSort(array)
 //largest value of two indices being compared will bubble up to it's top positon
 
 
+describe('bubbleSort', () => {
+
+it('should take an array as an argument', () => {
+expect()
+})
+
+it.only('should sort the array from smallest to largest', () => {
+  let array = [7, 12, 5, 42, 3];
+  
+  let expectedArray = [3, 5, 7, 12, 42];
+
+
+  expect(bubble(array)).to.deep.equal(expectedArray);
+})
+
+it('should include negative integers in a numerical array', () => {
+  let array = [7, 12, -2, 5, 42, 3, -75, -21];
+
+  let expectedArray = [-75, -21, -2, 3, 5, 7, 12, 42];
+
+  expect(bubbleSort(array).to.equal(expectedArray));
+})
+
+it('should be able to sort floating point integers', () => {
+  let array = []
+
+})
+
+it('should be able to sort letters', () => {
+
+})
+
+it('should be ')
+})
--- test/insertionSort-test.js
+++ test/insertionSort-test.js
@@ -1,3 +1,50 @@
-//should be a function insertionSort()
-//should take an array as an argument insertionSort(array)
-//
+
+import { expect } from 'chai';
+import { insertionSort } from '../lib/insertionSort.js';
+
+let insert = insertionSort
+
+describe('insertionSort', () => {
+
+it('should take an array as an argument', () => {
+expect()
+})
+
+it.only('should sort the array from smallest to largest', () => {
+  let array = [7, 12, 5, 42, 3];
+  
+  let expectedArray = [3, 5, 7, 12, 42];
+
+
+  expect(insertion(array)).to.deep.equal(expectedArray);
+})
+
+it('should include negative integers in a numerical array', () => {
+  let array = [7, 12, -2, 5, 42, 3, -75, -21];
+
+  let expectedArray = [-75, -21, -2, 3, 5, 7, 12, 42];
+
+  expect(insertion(array).to.equal(expectedArray));
+})
+
+it('should be able to sort floating point integers', () => {
+  let array = [87.2, 3.67, 22.43, 9.5, -7.6];
+
+  let expectedArray = [-7.6, 3.67, 9.5, -7.6];
+  expect(insertion(array).to.equal(expectedArray))
+})
+
+it('should be able to sort letters', () => {
+  let array = [a, q, y, c, t, f, o];
+
+  let expectedArray = [a, c, f, o, q, t, y];
+  expect(insertion(array).to.equal(expectedArray))
+})
+
+it('should place elements of the same value next to each other', () => {
+  let array = [7, 3, 5, 10, 3, 5, 6, 15, 5];
+
+  let expectedArray = [3, 3, 5, 5, 5, 6, 7, 10, 15];
+  expect(insertion(array).to.equal(expectedArray));
+})
+})
